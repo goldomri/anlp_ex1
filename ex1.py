021ce46f513dc102b041a7a772f8c2bd6c3dc8bd
goldomri <goldomri@gmail.com>
2025-05-01 09:31:32 +0300

    DataTrainingArguments, RunArguments

--- ex1.py
+++ ex1.py
@@ -9,18 +9,44 @@ TASK_NAME = "mrpc"
 
 
 @dataclass
-class MyArguments:
-    max_train_samples: int = field(default=-1, metadata={"help": "Limit train samples (–1 = all)"})
-    max_eval_samples: int = field(default=-1, metadata={"help": "Limit eval samples (–1 = all)"})
-    max_predict_samples: int = field(default=-1, metadata={"help": "Limit predict samples (–1 = all)"})
-    do_train: bool = field(default=False, metadata={"help": "Whether to run training"})
-    do_predict: bool = field(default=False, metadata={"help": "Whether to run prediction"})
-    model_path: str = field(default=None, metadata={"help": "Path to fine-tuned model for prediction"})
+class DataTrainingArguments:
+    max_train_samples: int = field(
+        default=-1,
+        metadata={"help": "Limit train samples (–1 = all)"}
+    )
+    max_eval_samples: int = field(
+        default=-1,
+        metadata={"help": "Limit eval samples (–1 = all)"}
+    )
+    max_predict_samples: int = field(
+        default=-1,
+        metadata={"help": "Limit predict samples (–1 = all)"}
+    )
+
+
+@dataclass
+class RunArguments:
+    do_train: bool = field(
+        default=False,
+        metadata={"help": "Whether to run training"}
+    )
+    do_predict: bool = field(
+        default=False,
+        metadata={"help": "Whether to run prediction"}
+    )
+    model_path: str = field(
+        default=None,
+        metadata={"help": "Path to fine-tuned model for prediction"}
+    )
 
 
 def parse_args():
-    parser = HfArgumentParser((MyArguments, TrainingArguments))
-    my_args, training_args = parser.parse_args_into_dataclasses()
+    """
+    Parse command line arguments.
+    :return: my_args, training_args
+    """
+    parser = HfArgumentParser((DataTrainingArguments, RunArguments, TrainingArguments))
+    data_args, run_args, training_args = parser.parse_args_into_dataclasses()
 
 
 def load_model_and_dataset():
@@ -44,3 +70,12 @@ def split_dataset(ds):
     val_set = ds['validation']
     test_set = ds['test']
     return train_set, val_set, test_set
+
+
+def main():
+    tokenizer, model, ds = load_model_and_dataset()
+
+    def preprocess_function(examples):
+        # Tokenize the texts
+        result = tokenizer(examples[‘input’], max_length = 512, truncation = True)
+        return result
