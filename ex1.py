bed0dbddf4fc3cc199935a8635f9d738db009a38
goldomri <goldomri@gmail.com>
2025-05-01 08:10:19 +0300

    functions for loading model and dataset and splitting the dataset

--- ex1.py
+++ ex1.py
@@ -2,12 +2,29 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from datasets import load_dataset
 
-
 MODEL_NAME = "bert-base-uncased"
 DATASET_NAME = "nyu-mll/glue"
 TASK_NAME = "mrpc"
 
+def load_model_and_dataset():
+    """
+    Loads the model (with the relevant tokenizer) and dataset from HuggingFace.
+    :return: tokenizer, model, dataset
+    """
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+    ds = load_dataset(DATASET_NAME, TASK_NAME)
+    return tokenizer, model, ds
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
-ds = load_dataset(DATASET_NAME, TASK_NAME)
+def split_dataset(ds):
+    """
+    Splits dataset into train, validation and test sets.
+    :param ds: The dataset to split.
+    :return: train_set, val_set, test_set
+    """
+    train_set = ds['train']
+    val_set = ds['validation']
+    test_set = ds['test']
+    return train_set, val_set, test_set
+
+
